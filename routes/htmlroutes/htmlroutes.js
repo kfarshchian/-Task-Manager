3b002574518a585cbc9eaa26ca2c7d546cac592e
Kameron <kfarshchian@gmail.com>
2023-03-13 17:12:32 -0600

    added notes

--- routes/htmlroutes/htmlroutes.js
+++ routes/htmlroutes/htmlroutes.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const notesEdit = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-
+//post the note route with uuid added.
 notesEdit.post('/', (req, res) => {
     const {title, text} = req.body;
     const db = JSON.parse(fs.readFileSync('./db/db.json', (err) => { if (err) console.log(err)}));
@@ -14,12 +14,12 @@ notesEdit.post('/', (req, res) => {
     fs.writeFileSync('./db/db.json', JSON.stringify(db, null, 2));
     res.json(item);
 });
-
+//get route
 notesEdit.get('/', (req, res) => {
     const db = fs.readFileSync('./db/db.json', (err) => { if (err) console.log(err)});
     res.json(JSON.parse(db));
 });
-
+//delete route with uuid as guide for what to delete.
 notesEdit.delete('/:id', (req, res) => {
     const db = JSON.parse(fs.readFileSync('./db/db.json', (err) => { if (err) console.log(err)}));
     const newDb = db.filter((note) => {
